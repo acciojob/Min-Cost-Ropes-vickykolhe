0ab94d5b885680487725382d342c55c3c1adf596
Babusha Kolhe <165826953+vickykolhe@users.noreply.github.com>
2025-04-07 23:01:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function mincost(arr)
 { 
 //write your code here
+	let cost=0;
+	let sum=0;
+	arr.sort();
+	arr.forEach((e)=>{
+		sum+=e;
+		cost+=sum;
+	})
 // return the min cost
+	return cost-2;
+	
   
 }
 
